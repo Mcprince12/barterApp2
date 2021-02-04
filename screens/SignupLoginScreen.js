cc9bacec76f521600de15d90a80a2a715df889eb
Mcprince12 <sohumtrip@gmail.com>
2021-02-04 11:45:03 +0200

    Modal not working

--- screens/SignupLoginScreen.js
+++ screens/SignupLoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Modal} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Modal, KeyboardAvoidingView} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import AppHeader from '../AppHeader'
 import firebase from 'firebase';
 import db from '../config'
@@ -11,7 +12,7 @@ export default class SignupLoginScreen extends React.Component{
         this.state={
             username:'',
             password:'',
-            isVisible:false,
+            isVisible: false,
             firstName:'',
             lastName:'',
             phoneNumber:'',
@@ -22,10 +23,20 @@ export default class SignupLoginScreen extends React.Component{
 
     showModal=()=>{
       <Modal
-            animationType="fade"
+           animationType="fade"
             transparent={true}
-            visible={this.state.isVisible=true}
+            visible={this.state.isVisible}
             >
+                <View style={styles.modalContainer}>
+                    <ScrollView style={{width:'100%'}}>
+                        <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+               
+                        <Text
+                            style={styles.modalTitle}
+                            >
+                                Registration
+                            </Text>
+               
                 <TextInput
                     style={styles.formTextInput}
                     placeholder={"First Name"}
@@ -129,7 +140,11 @@ export default class SignupLoginScreen extends React.Component{
                         </Text>
                     </TouchableOpacity>
                 </View>
+                </KeyboardAvoidingView>
+                </ScrollView>
+                </View>
             </Modal>
+            
     }
     userLogin = (username, password) =>{
         firebase.auth().signInWithEmailAndPassword(username, password)
@@ -179,7 +194,10 @@ export default class SignupLoginScreen extends React.Component{
     
     render(){
         return(
-            <View>
+            <View style={styles.container}>
+                 <View style={{justifyContent:'center', alignItems:'center'}}>
+                   {this.showModal()}
+               </View>
                 <AppHeader/>
                 <View style={{alignItems:'center',}}>
                     <Image source={require('../assets/barterr.png')}
@@ -222,10 +240,9 @@ export default class SignupLoginScreen extends React.Component{
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={()=>{
-                    <View style={{justifyContent:'center', alignItems:'center'}}>
-                        {this.showModal()}
-                    </View>
+                   
                     this.userSignUp(this.state.username, this.state.password)
+                    
                 }}
                 
                 style={styles.button}>
@@ -239,4 +256,4 @@ export default class SignupLoginScreen extends React.Component{
     }
 }
 
-const styles = StyleSheet.create({ container:{ flex:1, backgroundColor:'#F8BE85', alignItems: 'center', justifyContent: 'center' }, profileContainer:{ flex:1, justifyContent:'center', alignItems:'center', }, title :{ fontSize:65, fontWeight:'300', paddingBottom:30, color : '#ff3d00' }, loginBox:{ width: 300, height: 40, borderBottomWidth: 1.5, borderColor : '#ff8a65', fontSize: 20, margin:10, paddingLeft:10 }, KeyboardAvoidingView:{ flex:1, justifyContent:'center', alignItems:'center' }, modalTitle :{ justifyContent:'center', alignSelf:'center', fontSize:30, color:'#ff5722', margin:50 }, modalContainer:{ flex:1, borderRadius:20, justifyContent:'center', alignItems:'center', backgroundColor:"#ffff", marginRight:30, marginLeft : 30, marginTop:80, marginBottom:80, }, formTextInput:{ width:"75%", height:35, alignSelf:'center', borderColor:'#ffab91', borderRadius:10, borderWidth:1, marginTop:20, padding:10 }, registerButton:{ width:200, height:40, alignItems:'center', justifyContent:'center', borderWidth:1, borderRadius:10, marginTop:30 }, registerButtonText:{ color:'#ff5722', fontSize:15, fontWeight:'bold' }, cancelButton:{ width:200, height:30, justifyContent:'center', alignItems:'center', marginTop:5, }, button:{ width:300, height:50, justifyContent:'center', alignItems:'center', borderRadius:25, backgroundColor:"#ff9800", shadowColor: "#000", shadowOffset: { width: 0, height: 8, }, shadowOpacity: 0.30, shadowRadius: 10.32, elevation: 16, padding: 10 }, buttonText:{ color:'#ffff', fontWeight:'200', fontSize:20 } })
+const styles = StyleSheet.create({ container:{   backgroundColor:'#F8BE85', alignItems: 'center', justifyContent: 'center' }, profileContainer:{ flex:1, justifyContent:'center', alignItems:'center', }, title :{ fontSize:65, fontWeight:'300', paddingBottom:30, color : '#ff3d00' }, loginBox:{ width: 300, height: 40, borderBottomWidth: 1.5, borderColor : '#ff8a65', fontSize: 20, margin:10, paddingLeft:10 }, KeyboardAvoidingView:{ flex:1, justifyContent:'center', alignItems:'center' }, modalTitle :{ justifyContent:'center', alignSelf:'center', fontSize:30, color:'#ff5722', margin:50 }, modalContainer:{ flex:1, borderRadius:20, justifyContent:'center', alignItems:'center', backgroundColor:"#ffff", marginRight:30, marginLeft : 30, marginTop:80, marginBottom:80, }, formTextInput:{ width:"75%", height:35, alignSelf:'center', borderColor:'#ffab91', borderRadius:10, borderWidth:1, marginTop:20, padding:10 }, registerButton:{ width:200, height:40, alignItems:'center', justifyContent:'center', borderWidth:1, borderRadius:10, marginTop:30 }, registerButtonText:{ color:'#ff5722', fontSize:15, fontWeight:'bold' }, cancelButton:{ width:200, height:30, justifyContent:'center', alignItems:'center', marginTop:5, }, button:{ width:300, height:50, justifyContent:'center', alignItems:'center', borderRadius:25, backgroundColor:"#ff9800", shadowColor: "#000", shadowOffset: { width: 0, height: 8, }, shadowOpacity: 0.30, shadowRadius: 10.32, elevation: 16, padding: 10 }, buttonText:{ color:'#ffff', fontWeight:'200', fontSize:20 } })
